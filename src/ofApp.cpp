92f91159a9b217c8057820ce5476a7cc11eae03d
Emely Rosbach <emelyrosbach@roam216127.roam.fh-nuernberg.de>
2019-10-23 16:17:16 +0200

    initial version

---
+++ src/ofApp.cpp
@@ -0,0 +1,151 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    ofSetVerticalSync(true);
+    soundStream.printDeviceList();
+    
+    int bufferSize = 256;
+    
+    left.assign(bufferSize, 0.0);
+    right.assign(bufferSize, 0.0);
+    
+    
+    bufferCounter    = 0;
+    drawCounter        = 0;
+    smoothedVol     = 0.0;
+    scaledVol        = 0.0;
+    
+    
+    ofSoundStreamSettings settings;
+    
+    auto devices = soundStream.getMatchingDevices("default");
+    if(!devices.empty()){
+        settings.setInDevice(devices[0]);
+    }
+    
+    settings.setInListener(this);
+    settings.sampleRate = 44100;
+    settings.numOutputChannels = 0;
+    settings.numInputChannels = 2;
+    settings.bufferSize = bufferSize;
+    soundStream.setup(settings);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    //lets scale the vol up to a 0-1 range
+    scaledVol = ofMap(smoothedVol, 0.0, 0.17, 0.0, 1.0, true);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofRectangle rect;
+    rect.x = 10;
+    rect.y = 10;
+    rect.width = 100;
+    rect.height = 100;
+    ofSetColor(0,255,0);
+    
+    ofDrawRectangle(rect);
+    if(curVol>=S_TRIGGER){
+        ofSetColor(255,165,0);
+        ofDrawRectangle(rect);
+    }
+    
+    if(curVol>=0.1){
+        ofSetColor(255,0,0);
+        ofDrawRectangle(rect);
+    }
+    
+    ofDrawBitmapString(ofToString(curVol,2), 200, 200);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
+
+void ofApp::audioIn(ofSoundBuffer & input){
+    
+    curVol = 0.0;
+    
+    // samples are "interleaved"
+    int numCounted = 0;
+    
+    //lets go through each sample and calculate the root mean square which is a rough way to calculate volume
+    for (size_t i = 0; i < input.getNumFrames(); i++){
+        left[i]        = input[i*2]*0.5;
+        right[i]    = input[i*2+1]*0.5;
+        
+        curVol += left[i] * left[i];
+        curVol += right[i] * right[i];
+        numCounted+=2;
+    }
+    
+    //this is how we get the mean of rms :)
+    curVol /= (float)numCounted;
+    
+    // this is how we get the root of rms :)
+    curVol = sqrt( curVol );
+    
+    smoothedVol *= 0.93;
+    smoothedVol += 0.07 * curVol;
+    
+    bufferCounter++;
+    
+}
+
+
